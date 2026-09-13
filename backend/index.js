2d72ea78c196ecc27ee9c50f9c1766217d0d6ffa
Muhammad-Alee81 <heymaleehere@gmail.com>
2026-09-13 17:59:01 +0500

    added docker-compose.yml file

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,16 @@ configDotenv();
 const app = express();
 const port = process.env.PORT;
 
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    status: "success",
+    message: "user registered successfully",
+    data: {
+      name: "Muhammad Alee",
+    },
+  });
+});
+
 app.listen(port, () => {
-  console.log(`server is rnning on port ${port}`);
+  console.log(`server is runing on port ${port}`);
 });
